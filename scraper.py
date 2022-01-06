be29ccc26d568e4bc66c37f556698224d626d56d
Your Name <you@example.com>
2022-01-06 09:23:03 +0530

    auto

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from tabula import read_pdf
 
 
 import os,requests,time,bs4,datetime,csv;
